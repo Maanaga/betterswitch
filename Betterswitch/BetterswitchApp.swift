2dac37e24aa856663cad303a1fe30942c19346d9
Luka Managadze <managaluka0@gmail.com>
2026-06-24 02:37:20 +0400

    Initial Commit

---
+++ Betterswitch/BetterswitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterswitchApp.swift
+//  Betterswitch
+//
+//  Created by Luka Managadze on 24/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterswitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
